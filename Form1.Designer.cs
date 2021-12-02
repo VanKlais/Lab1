4a347e705e42e9f6dfc2d3c752d513de06c0af1d
Цой Артем <coi_arti@list.ru>
2021-12-02 08:22:50 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace Lab1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(25, 123);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 29);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(49, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(39, 23);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "26";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(20, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "N";
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(420, 29);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(394, 728);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Буква";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Вероятность";
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Кодовая комбинация";
+            this.Column3.Name = "Column3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(134, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(27, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "N";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(231, 60);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(27, 25);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "N";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(20, 90);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(26, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "R";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(49, 91);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(39, 23);
+            this.textBox2.TabIndex = 6;
+            this.textBox2.Text = "100";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(885, 450);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Text = "";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            label2.Text = "";
+            label3.Text = "";
+            int size = Convert.ToInt32(textBox1.Text);
+            char[] alphabet = new char[size];
+            double[] countLet = new double[size];
+            string[] codeCom = new string[size];
+            dataGridView1.RowCount = size;
+            Random rnd = new Random();
+
+            char sup;
+            int t = -1;
+            while (true)
+            {
+                if (t == size - 1)
+                {
+                    break;
+                }
+                sup = Convert.ToChar(rnd.Next(95, 127));
+                if (!alphabet.Contains(sup))
+                {
+                    t++;
+                    alphabet[t] = sup;
+                    label2.Text += $"{t + 1})\t" + alphabet[t] + Environment.NewLine;
+                }
+            }
+
+            double rndUser = Convert.ToInt32(textBox2.Text);
+
+            double prop = 100.0 / rndUser;
+            for (double con = 100.0 / size; con < 100 + prop; con += prop)
+            {
+
+                countLet[rnd.Next(0, size)] += Math.Round(prop, 2);
+            }
+
+            for (int i = 0; i < size; i++)
+            {
+                label3.Text += $"{i + 1})\t" + countLet[i] + Environment.NewLine;
+            }
+
+            double sucountLet;
+            char sup2;
+            for (int i = 0; i < size - 1; i++)
+            {
+                for (int j = i + 1; j < size; j++)
+                {
+                    if (countLet[i] < countLet[j])
+                    {
+                        sucountLet = countLet[i];
+                        countLet[i] = countLet[j];
+                        countLet[j] = sucountLet;
+
+                        sup2 = alphabet[i];
+                        alphabet[i] = alphabet[j];
+                        alphabet[j] = sup2;
+                    }
+                }
+            }
+            int length=0;
+            for (int i = 0; i < size-1; i++)
+            {
+                if (countLet[i] > 0)
+                    length++;
+                else break;
+            }
+                Fano(0, size - 1);
+            
+            for (int j = 0; j < size; j++)
+            {
+                dataGridView1.Rows[j].Cells[0].Value = alphabet[j];
+                dataGridView1.Rows[j].Cells[1].Value = countLet[j];
+                dataGridView1.Rows[j].Cells[2].Value = codeCom[j];
+            }
+
+            void Fano(int L, int R)
+            {
+                int n = L;
+                double sum = 0;
+                if (L < R)
+                {
+                    for (int j = L; j < R + 1; j++)
+                    {
+                        sum += countLet[j];
+                    }
+                    double mid = sum / 2;
+
+                    for (int i = L; i <= R; i++)
+                    {
+                        if ((countLet[i] == 0)) break;
+                        if (R - L == 1)
+                        {
+                            n++;
+                            codeCom[L] += "0";
+                            codeCom[R] += "1";
+                            break;
+                        }
+                        else if (countLet[i] <= mid)
+                        {
+                            n++;
+                            mid -= countLet[i];
+                            codeCom[i] += "0";
+                        }
+                        else
+                        {
+                            mid = 0;
+                            codeCom[i] += "1";
+                        }
+                    }
+                    Fano1(L, n - 1);
+                    Fano(n, R);
+                }
+            }
+            void Fano1(int L, int R)
+            {
+                int n = L;
+                double sum = 0;
+                if (L < R)
+                {
+                    for (int j = L; j < R + 1; j++)
+                    {
+                        sum += countLet[j];
+                    }
+                    double mid = sum / 2;
+
+                    for (int i = L; i <= R; i++)
+                    {
+                        if ((countLet[i] == 0)) break;
+                        if (R - L == 1)
+                        {
+                            n++;
+                            codeCom[L] += "0";
+                            codeCom[R] += "1";
+                            break;
+                        }
+                        else if (countLet[i] <= mid)
+                        {
+                            n++;
+                            mid -= countLet[i];
+                            codeCom[i] += "0";
+                        }
+                        else
+                        {
+                            mid = 0;
+                            codeCom[i] += "1";
+                        }
+                    }
+                    Fano(L, n - 1);
+                    Fano1(n, R);
+                }
+            }
+        }
+    }
+}
